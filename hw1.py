9f62a519dd1c8c351af6e90929f1e170dacc8621
wooren <127368021+wooren@users.noreply.github.com>
2023-03-09 20:47:50 +0900

    Create hw1.py

---
+++ hw1.py
@@ -0,0 +1,10 @@
+def get_radius(prompt):
+  r = int(input(prompt);
+  return r;
+def get_circle_area(R):
+  area = R*R*3.14;
+  return area;
+prompt = ('whats r?')
+result = get_radius(prompt);
+ending = get_circle_area(result);
+print(result,'x',result,'=',ending);
